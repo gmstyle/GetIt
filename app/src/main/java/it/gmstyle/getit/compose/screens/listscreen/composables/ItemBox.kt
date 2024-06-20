6658e71029cbee84b7194dd5e7026caf1e553a75
Gabriele Martina <gabriele.martina@icsone.it>
2024-06-20 18:09:08 +0200

    Update ItemBox.kt

--- app/src/main/java/it/gmstyle/getit/compose/screens/listscreen/composables/ItemBox.kt
+++ app/src/main/java/it/gmstyle/getit/compose/screens/listscreen/composables/ItemBox.kt
@@ -8,7 +8,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.style.TextDecoration
 import it.gmstyle.getit.compose.composables.commons.CommonTextField
 import it.gmstyle.getit.data.entities.ListItem
@@ -19,10 +24,22 @@ fun ItemBox(
     onUpdateItem: (ListItem) -> Unit,
     onDelete: (ListItem) -> Unit,
 ) {
+    var hasBeenFocused by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf(item.name) }
     Row {
         CommonTextField(
-            modifier = Modifier.weight(1f),
-            value = item.name,
+            modifier = Modifier
+                .weight(1f)
+                .onFocusChanged { focusState ->
+                    hasBeenFocused =  hasBeenFocused || focusState.isFocused
+                    if (!focusState.isFocused && hasBeenFocused && itemName.isNotEmpty()) {
+                        if (item.name != itemName) {
+                            onUpdateItem(item.copy(name = itemName))
+                        }
+                    }
+                }
+            ,
+            value = itemName,
             // quando item.completed mostra il testo sbarrato
             // se non mostra nulla
             enabled = !item.completed,
@@ -31,9 +48,7 @@ fun ItemBox(
                     textDecoration = TextDecoration.LineThrough
                 ) else MaterialTheme.typography.bodyLarge,
             onValueChange = { newName ->
-                if (newName != item.name && newName.isNotEmpty()) {
-                    onUpdateItem(item.copy(name = newName))
-                }
+                itemName = newName
             },
             leadingIcon = {
                 Checkbox(
